d2ec9e404c46b8db4ab2ab4c881cf730ab9b7f08
servicecitycloudservice <168943329+servicecitycloudservice@users.noreply.github.com>
2024-05-04 21:17:38 -0400

    Add files via upload

---
+++ app/app.js
@@ -0,0 +1,17 @@
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
+import { auth } from "./Environments/firebase-auth.js";
+import { loginCheck } from "./auth/login_Check.js";
+
+// Login
+import "./auth/login_Google.js";
+// import './auth/login_Check.js';
+// import "./auth/logout.js";
+import './auth/signin_Form.js';
+import "./auth/signup_Form.js";
+
+onAuthStateChanged(auth, async (user) => {
+    if (user) {
+        loginCheck(user);
+    } else {
+    }
+});
