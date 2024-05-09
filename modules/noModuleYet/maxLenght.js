4e88a72a27307e50758279ba67a2b987a0f0433a
servicecitycloudservice <168943329+servicecitycloudservice@users.noreply.github.com>
2024-05-08 20:49:07 -0500

    Add files via upload

---
+++ modules/noModuleYet/maxLenght.js
@@ -0,0 +1,6 @@
+    // Máximo de caracteres en las columnas
+    registerForm["nombre"].setAttribute("maxlength", "20");
+    registerForm["apodo"].setAttribute("maxlength", "20");
+    registerForm["whatsapp"].setAttribute("maxlength", "9");
+    registerForm["estado"].setAttribute("maxlength", "20");
+    registerForm["observacion"].setAttribute("maxlength", "200");
